b505ada5ee2c36a1b29a4aa79a296118aee94be7
AndreasFurster <andreas.furster@hotmail.com>
2015-04-02 14:30:29 +0200

    Created overload method and refactored

--- Lisa.Common/Lisa.Common/Lisa.Common/Access/Proxy.cs
+++ Lisa.Common/Lisa.Common/Lisa.Common/Access/Proxy.cs
@@ -11,18 +11,16 @@ namespace Lisa.Common.Access
 {
     public class Proxy<T>
     {
-        public Proxy(string baseUrl, string resourceUrl) : this(baseUrl, resourceUrl, null, null)
-        {
-        }
-
-        public Proxy(string baseUrl, string resourceUrl, string tokenType, string token)
+        public Proxy(string baseUrl, string resourceUrl)
         {
             _apiBaseUrl = baseUrl.Trim('/');
             _proxyResourceUrl = resourceUrl.Trim('/');
+            
             _httpClient = new HttpClient
             {
                 BaseAddress = new Uri(_apiBaseUrl)
             };
+            
             _jsonSerializerSettings = new JsonSerializerSettings
             {
                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
@@ -35,25 +33,28 @@ namespace Lisa.Common.Access
                     }
                 }
             };
+        }
 
-            Authorize(_httpClient, token, tokenType);
+        public Proxy(string baseUrl, string resourceUrl, string tokenType, string token) : this(baseUrl, resourceUrl)
+        {
+            if (token != null && tokenType != null)
+            {
+                _httpClient.DefaultRequestHeaders.Add("Authorization", String.Format("{0} {1}", tokenType, token));
+            }
         }
 
         public async Task<IEnumerable<T>> GetAsync()
         {
             var result = await _httpClient.GetAsync(_proxyResourceUrl);
-
-            var json = await result.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<IEnumerable<T>>(json, _jsonSerializerSettings);
+            return await DeserializeList(result);
         }
 
         public async Task<T> GetAsync(int id)
         {
             var url = String.Format("{0}/{1}", _proxyResourceUrl, id);
-            var result = await _httpClient.GetAsync(url);
 
-            var json = await result.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
+            var result = await _httpClient.GetAsync(url);
+            return await DeserializeSingle(result);
         }
 
         public async Task<T> PostAsync(T model)
@@ -66,9 +67,7 @@ namespace Lisa.Common.Access
             };
 
             var result = await _httpClient.SendAsync(request);
-            var json = await result.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
+            return await DeserializeSingle(result);
         }
 
         public async Task<T> PatchAsync(int id, T model)
@@ -81,18 +80,37 @@ namespace Lisa.Common.Access
             };
 
             var result = await _httpClient.SendAsync(request);
-            var json = await result.Content.ReadAsStringAsync();
+            return await DeserializeSingle(result);
+        }
+
+        public async Task<T> DeleteAsync(int id, T model)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = new HttpMethod("PATCH"),
+                RequestUri = new Uri(String.Format("{0}/{1}/{2}", _apiBaseUrl, _proxyResourceUrl, id)),
+                Content = new StringContent(JsonConvert.SerializeObject(model, _jsonSerializerSettings), Encoding.UTF8, "application/json")
+            };
+
+            var result = await _httpClient.SendAsync(request);
+            return await DeserializeSingle(result);
+        }
+
+        private async Task<T> DeserializeSingle(HttpResponseMessage response)
+        {
+            var json = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
         }
 
-        private void Authorize(HttpClient client, string token, string tokenType)
+        private async Task<IEnumerable<T>> DeserializeList(HttpResponseMessage response)
         {
-            if (token != null && tokenType != null)
-            {
-                client.DefaultRequestHeaders.Add("Authorization", String.Format("{0} {1}", tokenType, token));
-            }
+            var json = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(json, _jsonSerializerSettings);
         }
+
+
 
         private readonly HttpClient _httpClient;
         private readonly string _apiBaseUrl;
