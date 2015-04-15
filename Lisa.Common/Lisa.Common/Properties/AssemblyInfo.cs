35363487a88bcabb0d5f0fb3ae1813183a1f37fa
AndreasFurster <andreas.furster@hotmail.com>
2015-04-15 15:08:02 +0200

    To version 1.0.1

--- Lisa.Common/Lisa.Common/Properties/AssemblyInfo.cs
+++ Lisa.Common/Lisa.Common/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("Lisa.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Lisa")]
 [assembly: AssemblyProduct("Lisa.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -24,5 +24,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
